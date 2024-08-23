aebcbf2d5b4ffdeb4d1eff5f7bf6f6fa6ee180f3
LYH <dudgk7829@naver.com>
2024-08-23 16:50:03 +0900

    20240823 16:50

--- src/main/java/com/lyh/springbasic/service/implement/BasicServiceImplement.java
+++ src/main/java/com/lyh/springbasic/service/implement/BasicServiceImplement.java
@@ -2,9 +2,19 @@ package com.lyh.springbasic.service.implement;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+// import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.lyh.springbasic.service.BasicService;
 
+// Service 레이어
+// - 실제 비즈니스 로직을 실행하는 영역
+// - 트랜잭션(기능 단위)처리나 유효성 검사(이전 트랜잭션 결과의 유효성 검사)를 수행함 
+// - Controller로부터 받은 요청을 처리하고 해당 저리 중 데이터베이스 작업이 필요하면 Repository 영역으로 요청
+
+// @Service : 해당 클래스를 Spring Bean으로 등록하는 어노테이션, @Component와 동일하지만 각동성 향상을 위해 Service 이름을 사용
+// @Component
+@Service
 public class BasicServiceImplement implements BasicService {
 
     @Override
