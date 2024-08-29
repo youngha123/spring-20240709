8d74f53bec910fc2c2aa9529ee8be8e5e1ebf528
LYH <dudgk7829@naver.com>
2024-08-29 16:50:56 +0900

    20240829 16:50

--- src/main/java/com/lyh/springbasic/provider/JwtProvider.java
+++ src/main/java/com/lyh/springbasic/provider/JwtProvider.java
@@ -6,6 +6,7 @@ import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
@@ -22,13 +23,33 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtProvider {
 
+    // JWT 암호화에 사용되는 비밀키는 보안 관리가 되어야함
+    // 코드에 직접적으로 작성하면 보안상 좋지 않음
+
+    // 해결책
+    // 1. application.properties / application.yaml에 등록
+    // - application.properties / application.yaml에 비밀키를 작성
+    // - @Value()를 이용하여 값을 가져옴
+    // - 주의사항 : application.properties / application.yaml을 .gitignore에 등록해야함
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    // 2. 시스템의 환경변수로 등록하여 사용
+    // - OS 자체의 시스템 환경변수에 비밀키로 등록
+    // - Spring에서 환경변수 값을 읽어서 사용
+
+    // 3. 외부 데이터 관리 도구를 사용
+    // - 자체 서버가 아닌 타 서버에 등록된 Vault 도구를 사용하여 비밀키 관리
+    // - OS 부팅 시 Vault 서버에서 비밀키를 가져와 사용
+    // - OS 매번 부팅 시 새로운 비밀키를 부여함
+
     public String create(String name) {
 
         // JWT의 만료일자 및 시간
         Date expiredDate = Date.from(Instant.now().plus(4, ChronoUnit.HOURS));
 
         // 비밀키 생성
-        String secretKey = "qwer1234";
+        // String secretKey = "qwertyuiasdfghjklzxcvbnm123456789012345678901234567980";
         Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 
         // JWT 생성
